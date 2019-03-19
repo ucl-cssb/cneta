e0ab435bdd1b8cb2525d19ea718f2e2582e2ba1f
cpbarnes <christopher.barnes@ucl.ac.uk>
2019-03-19 18:38:20 +0000

    removed comments

--- run-svtreeml.sh
+++ run-svtreeml.sh
@@ -10,8 +10,8 @@ Npop=100
 Ngen=50
 Nstop=3
 
-#code/svtreeml -c $input -t $times -s $Ns -p $Npop -g $Ngen -e $Nstop -o results-maxL-tree.txt
-#Rscript ana/plot-trees-single.R results-maxL-tree.txt 0 #>& /dev/null
+code/svtreeml -c $input -t $times -s $Ns -p $Npop -g $Ngen -e $Nstop -o results-maxL-tree.txt
+Rscript ana/plot-trees-single.R results-maxL-tree.txt 0 #>& /dev/null
 
 code/svtreemu -c $input -t $times -p results-maxL-tree.txt -s $Ns -e 1.0 -l 0.5 -o results-maxL-mu-tree.txt
 Rscript ana/plot-trees-single.R results-maxL-mu-tree.txt 0 #>& /dev/null
